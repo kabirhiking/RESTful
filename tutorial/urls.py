fe17c467eb5990b6b63af7b83fa0ad4a1171c4f7
kabir <kabirraihan249@gmail.com>
2024-11-23 02:47:20 +0600

    Serialization

--- tutorial/urls.py
+++ tutorial/urls.py
@@ -15,6 +15,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.urls import include, path
+from django.contrib import admin
 from rest_framework import routers
 
 from tutorial.quickstart import views
@@ -27,5 +28,6 @@ router.register(r'groups', views.GroupViewSet)
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
     path('', include(router.urls)),
+    path("admin/", admin.site.urls),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
